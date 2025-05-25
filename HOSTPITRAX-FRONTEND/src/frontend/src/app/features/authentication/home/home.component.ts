18723ee9dcfeaffe6b733885ec7971d43e5a686b
md_sohag <sohag.ali@welldev.io>
2025-05-25 20:22:40 +0600

    HT-01 - home page added - routing constant file added

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/features/authentication/home/home.component.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/features/authentication/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatButton
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
