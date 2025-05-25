3b3ccd0f7e043f384f29feedabd17e00cf32fb90
md_sohag <sohag.ali@welldev.io>
2025-05-25 16:07:42 +0600

    HT-01 - home page added - routing constant file added

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/app.routes.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./features/authentication/home/home.component";
+import {ROUTE_LINK} from "./core/constants/route.link";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ROUTE_LINK.HOME.link,
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: ROUTE_LINK.HOME.link,
+    pathMatch: 'full'
+  }
+];
---
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/core/constants/route.link.ts
@@ -0,0 +1,7 @@
+export const PATH_VARIABLE = {
+  BASE: 'hospitrax/web',
+};
+
+export const ROUTE_LINK = {
+  HOME: { link: `${PATH_VARIABLE.BASE}/home` },
+};
