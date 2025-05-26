6665e79ef4c5cffc2a59a71598730f320d05c8f7
md_sohag <sohag.ali@welldev.io>
2025-05-26 15:08:59 +0600

    HT-01 - header completed

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/core/constants/layout/header/main-navigation/main-navigation.component.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/core/constants/layout/header/main-navigation/main-navigation.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'main-navigation',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    MatButton,
+    NgStyle
   ],
   templateUrl: './main-navigation.component.html',
   styleUrl: './main-navigation.component.css'
