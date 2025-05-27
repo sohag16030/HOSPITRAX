eb5e77a199aeb8277a80d53777c6f7fe1e873157
md_sohag <sohag.ali@welldev.io>
2025-05-27 17:27:07 +0600

    HT-02 - breadcrumb-navigation shown on the hero-banner footer

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/features/lorem/hero-banner/hero-banner.component.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/features/lorem/hero-banner/hero-banner.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {BreadcrumbNavigationComponent} from "../breadcrumb-navigation/breadcrumb-navigation.component";
 
 @Component({
   selector: 'hero-banner',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    BreadcrumbNavigationComponent
   ],
   templateUrl: './hero-banner.component.html',
   styleUrl: './hero-banner.component.css'
