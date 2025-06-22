78c9711bebbbfa927f75fd521801f90af21c2dba
md_sohag <sohag.ali@welldev.io>
2025-06-22 21:31:09 +0600

    HT-04 - doctors list filter menu prepared

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/features/doctor/search-card/search-card.component.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/features/doctor/search-card/search-card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
-import {NgStyle} from "@angular/common";
+import {NgClass, NgStyle} from "@angular/common";
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {FilterCardComponent} from "../filter-card/filter-card.component";
 import {DoctorDetailsCardComponent} from "../doctor-details-card/doctor-details-card.component";
 import {DoctorListComponent} from "../doctor-list/doctor-list.component";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'search-card',
@@ -16,11 +17,13 @@ import {DoctorListComponent} from "../doctor-list/doctor-list.component";
     MatCardContent,
     FilterCardComponent,
     DoctorDetailsCardComponent,
-    DoctorListComponent
+    DoctorListComponent,
+    MatButton,
+    NgClass
   ],
   templateUrl: './search-card.component.html',
   styleUrl: './search-card.component.css'
 })
 export class SearchCardComponent {
-
+  selectedTab = 'all';
 }
