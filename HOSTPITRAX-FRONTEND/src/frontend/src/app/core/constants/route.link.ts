c94d870f76ad1d01d43706c730cca60101afa92d
md_sohag <sohag.ali@welldev.io>
2025-07-06 21:28:09 +0600

    HT-04 - removed unnecessary cards

--- HOSTPITRAX-FRONTEND/src/frontend/src/app/core/constants/route.link.ts
+++ HOSTPITRAX-FRONTEND/src/frontend/src/app/core/constants/route.link.ts
@@ -5,4 +5,5 @@ export const PATH_VARIABLE = {
 export const ROUTE_LINK = {
   HOME: { link: `${PATH_VARIABLE.BASE}/lorem` },
   FIND_A_DOCTOR: { link: `${PATH_VARIABLE.BASE}/doctors` },
+  MEDICAL_SERVICES: { link: `${PATH_VARIABLE.BASE}/medical-services` },
 };
